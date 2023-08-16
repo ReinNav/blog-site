f3edd840512c82cf9773ca7721fdd823a047b48d
Reinardus Navellio <reintirta@gmail.com>
2023-08-16 19:34:43 +0200

    Added absolute url function for post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     """Custom manager to retrieve all PUBLISHED posts"""
@@ -13,7 +14,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
@@ -32,3 +33,5 @@ class Post(models.Model):
         ]
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
