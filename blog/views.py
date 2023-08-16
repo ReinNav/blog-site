bd4fae6f4573752191d9dad6940d07a1215b07bd
Reinardus Navellio <reintirta@gmail.com>
2023-08-16 10:38:36 +0200

    Added post detail template

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,5 @@ def post_list(request):
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post':post})
+    return render(request, 'blog/post/detail.html', {'post': post})
 
